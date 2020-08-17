02adc763926f4569fae85cd8751423ed5a146014
Brenson <brensonw300@gmail.com>
2020-08-17 16:52:50 -0700

    Finished 3 tasks working on 4th

--- index.js
+++ index.js
@@ -27,11 +27,17 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ *  counter1 has closures so variables cant go outside function and counter2 closure is placed outside the function and has variables that we can access on global.
  * 
  * 2. Which of the two uses a closure? How can you tell?
+ * they do both use closure but counter1 isn't outside of the function while counter2 does go outside
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ * so counter1 is faster, the code is smaller and condensed plus 
+ *    we wouldn't have to access global variables to use the function. 
+ * 
+ * counter2 could be good only if the variables we needed were on the global scope
+ *  the same variable would not be accessible as a parameter with its global scope closure
 */
 
 // counter1 code
@@ -56,11 +62,13 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(points){
 
-    /*Code Here*/
+ return ` ${Math.floor(Math.random()* Math.floor(points))}` ;
 
 }
+
+console.log(inning(3));
 
 /* Task 3: finalScore()
 
@@ -76,11 +84,21 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore( inning, numberOfInnings){
+let result ={
+'home': 0,
+'away': 0
 }
+for(let i = 0; i <= numberOfInnings; i++){
+  result.home += inning();
+  result.away += inning();
+}
+return result;
+}
+console.log(finalScore(inning, 9));
+
+
+ 
 
 /* Task 4: 
 
@@ -102,9 +120,34 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
+// function scoreboard(finalScore, inning, num){
+// let home = 0;
+// let away = 0;
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
-}
+// for(i = 0; i<num; i++){
+// home += finalScore(inning, i).home;
+// away += finalScore(inning, i).away;
+// console.log(`1st inning: ${away} - ${home}`)
+// }
+
+  
+
+
+
+
+// }
+
+
+// function inning(){
+//   return (Math.floor(Math.random()* 3))
+// }
+// function getInningScore(callback1, inning){
+//   let scores = [];
+//   for(let i = 0; i < inning; i++){
+//     scores.push({'Home': callback1(), 'Away': callback1()});
+//   }
+//   return (scores);
+// }
+// console.log(scoreboard(finalScore, inning, 10));
 
 
